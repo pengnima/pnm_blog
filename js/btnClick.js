e72e034bf02839be24368e4ef4a57185b7c5746f
peng <peng@qq.com>
2020-04-23 21:45:11 +0800

    右键色图

--- js/btnClick.js
+++ js/btnClick.js
@@ -9,3 +9,22 @@ $navBtn.addEventListener("click", function () {
     $bar.className = "nav_bar";
   }
 });
+
+//控制切换页面
+import page from "./Page.js";
+let $page = document.querySelector(".blog_page.wrap");
+$page.addEventListener("click", function (e) {
+  let ev = e || window.event;
+  let dom = ev.target;
+
+  if (dom.className.indexOf("fa-arrow-left") != -1) {
+    page.pre();
+    console.log(1);
+  } else if (dom.className.indexOf("fa-arrow-right") != -1) {
+    page.next();
+  } else {
+    if (Number(dom.innerText)) {
+      page.selectPage(dom.innerText);
+    }
+  }
+});
